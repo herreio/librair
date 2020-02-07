abb19ef4a263a4e8f451a08f0211ab0bc1dfbd3b
herreio <git@herre.io>
2020-02-07 18:56:46 +0100

    initial

---
+++ README.rst
@@ -0,0 +1,49 @@
+.. role:: shell(code)
+   :language: shell
+
+Librair – Library Information Retrieval
+=======================================
+
+Introduction
+------------
+
+This is a package for harvesting data from different library catalogs.
+
+Installation
+------------
+
+You can simply install this package via `PyPI <https://pypi.org/project/librair/>`_:
+
+.. code-block:: shell
+
+    pip install librair
+
+... or by cloning the repository:
+
+.. code-block:: shell
+
+    git clone https://github.com/herreio/librair.git
+    cd librair
+
+
+Documentation
+-------------
+
+A minimal documentation can be found on `Read the Docs <https://librair.readthedocs.io/>`_.
+
+To build the documentation from the files found at docs:
+
+.. code-block:: shell
+
+    cd docs
+    make html
+
+
+Usage
+-----
+
+Launch the Python interpreter and start by importing the necessary modules:
+
+.. code-block:: python
+
+    from librair import *
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import setuptools
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(ROOT, 'README.rst')).read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="librair",
+    version="2020.02.07",
+    author="Donatus Herre",
+    author_email="pypi@herre.io",
+    license="MIT",
+    description="Library Information Retrieval",
+    long_description=README,
+    url="https://github.com/herreio/librair",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=required,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
