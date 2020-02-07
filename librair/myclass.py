16f6a61ecc3730fc1bbf90cd6eaa607f797d73b8
herreio <git@herre.io>
2020-02-07 19:04:57 +0100

    librair/ myclass added

---
+++ librair/myclass.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+description
+"""
+
+
+class MyClass:
+
+    def __init__(self):
+        """
+        MyClass documentation
+        """
+        pass
+
+    def method(self):
+        pass
