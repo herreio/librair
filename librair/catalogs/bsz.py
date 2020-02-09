2fcffe886ef668fe016dd1d9d9ba649b8559bd28
herreio <git@herre.io>
2020-02-09 16:38:22 +0100

    librair/catalogs/ fix typo

--- librair/catalogs/bsz.py
+++ librair/catalogs/bsz.py
@@ -14,5 +14,5 @@ def explain(store=False, path=""):
     if not store:
         xml.pretty(result)
     else:
-        file = xml.filepath("sru", "kobv", "", "")
+        file = xml.filepath("sru", "bsz", "", "")
         xml.writer(result, file, path=path)
