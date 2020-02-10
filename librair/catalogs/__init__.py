83f73dd00148947b40c24e7decabdc1585ba081f
herreio <git@herre.io>
2020-02-10 23:43:54 +0100

    librair/catalogs/ update module imports

--- librair/catalogs/__init__.py
+++ librair/catalogs/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from . import bsz, bvb, ddb, dnb, gbv, \
-    hebis, hbz, k10plus, kalliope, kobv, mdz, zdb
+from . import bvb, ddb, dnb, gbv, gnd, hebis, \
+    hbz, isil, k10plus, kalliope, kobv, mdz, swb, zdb
