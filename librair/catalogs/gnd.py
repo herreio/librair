544d83f540a7ddc59ca12a2fd6c4349c3e18bc65
herreio <git@herre.io>
2020-02-10 23:37:26 +0100

    librair/catalogs/ dnb updated, gnd and isil added

--- librair/catalogs/gnd.py
+++ librair/catalogs/gnd.py
@@ -4,9 +4,9 @@
 from ..protocols import sru
 from ..schemas import xml
 
-BASE = "https://services.dnb.de/sru/dnb"
+BASE = "https://services.dnb.de/sru/authorities"
 VERSION = "1.1"
-SCHEMA = "mods-xml"  # dc, RDFxml, PicaPlus-xml
+SCHEMA = "mods-xml"  # dc, RDFxml, PicaPlus-xml, baseDc-xml
 
 
 def explain(store=False, path=""):
@@ -14,5 +14,5 @@ def explain(store=False, path=""):
     if not store:
         xml.pretty(result)
     else:
-        file = xml.filepath("sru-explain", "dnb", "", "")
+        file = xml.filepath("sru-explain", "dnb-authorities", "", "")
         xml.writer(result, file, path=path)
--- librair/catalogs/isil.py
+++ librair/catalogs/isil.py
@@ -4,7 +4,7 @@
 from ..protocols import sru
 from ..schemas import xml
 
-BASE = "https://services.dnb.de/sru/dnb"
+BASE = "http://services.dnb.de/sru/bib"
 VERSION = "1.1"
 SCHEMA = "mods-xml"  # dc, RDFxml, PicaPlus-xml
 
@@ -14,5 +14,5 @@ def explain(store=False, path=""):
     if not store:
         xml.pretty(result)
     else:
-        file = xml.filepath("sru-explain", "dnb", "", "")
+        file = xml.filepath("sru-explain", "dnb-isil", "", "")
         xml.writer(result, file, path=path)
