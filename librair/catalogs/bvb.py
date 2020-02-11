5dd509b9f3750b9faa38039ec7396c81dc8d599a
herreio <git@herre.io>
2020-02-12 00:42:41 +0100

    librair/catalogs/ bvb updated

--- librair/catalogs/bvb.py
+++ librair/catalogs/bvb.py
@@ -9,25 +9,13 @@ BASE = "http://bvbr.bib-bvb.de:5661/bvb01sru"
 VERSION = "1.1"
 
 
-def _explain():
+def explain(store=False, path=""):
     """
     get sru explanation
     """
-    return sru.explain(BASE, VERSION)
-
-
-def explain():
-    """
-    print pretty sru explanation
-    """
-    result = _explain()
-    xml.pretty(result)
-
-
-def explain_store():
-    """
-    store sru explanation
-    """
-    result = _explain()
-    file = xml.filepath("sru", "bvb", "", "")
-    xml.writer(result, file, path="res/meta")
+    result = sru.explain(BASE, VERSION)
+    if not store:
+        xml.pretty(result)
+    else:
+        file = xml.filepath("sru", "bvb", "", "")
+        xml.writer(result, file, path=path)
