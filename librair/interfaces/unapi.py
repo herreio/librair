41dd23a9773b8aa807f906cb562b4c92f6b8d2ef
herreio <git@herre.io>
2020-03-01 12:52:22 +0100

    librair/interfaces/unapi.py updated

--- librair/interfaces/unapi.py
+++ librair/interfaces/unapi.py
@@ -6,12 +6,19 @@ from ..protocols import http
 
 class Client:
     """
-    unAPI client
+    unAPI client for database given by url and id
+    and items specified by given variable
     """
 
-    def __init__(self, URL):
+    def __init__(self, URL, DB, VAR):
         self.URL = URL
+        self.DB = DB
+        self.VAR = VAR
         self.formats = self._formats()
+
+    def _base(self, item, schema):
+        return self.URL + "/?id=" + self.DB + ":" + \
+                 self.VAR + ":" + item + "&format=" + schema
 
     def _formats(self):
         response = http.get_request(self.URL)
@@ -30,3 +37,24 @@ class Client:
             else:
                 formats[n]['docs'] = None
         return formats
+
+    def retrieve(self, item, schema):
+        """
+        retrieve data of item given by id, in given schema
+        """
+        if schema in self.formats:
+            schematype = self.formats[schema]['type']
+            url = self._base(item, schema)
+            response = http.get_request(url)
+            if response is not None:
+                if "xml" in schematype:
+                    return http.response_xml(response)
+                elif "json" in schematype:
+                    return http.response_json(response)
+                elif "plain" in schematype:
+                    return http.response_text(response)
+            else:
+                return response
+        else:
+            print("schema not supported!")
+            return None
