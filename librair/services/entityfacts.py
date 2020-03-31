6f32b710fb99dd6fb4d8d7ce5bd2cfe4311584ec
herreio <git@herre.io>
2020-03-31 02:06:30 +0200

    librair/services/entityfacts.py updated (store)

--- librair/services/entityfacts.py
+++ librair/services/entityfacts.py
@@ -3,7 +3,12 @@
 
 from ..protocols import http
 
+from urllib.request import urlretrieve
+
 BASE = "https://hub.culturegraph.org/entityfacts"
+
+PND = "116473207"
+GKD = "104526539X"
 
 
 def address(idn):
@@ -20,3 +25,14 @@ def request(idn):
     url = address(idn)
     res = http.get_request(url)
     return http.response_json(res)
+
+
+def store(idn, path="."):
+    """
+    request data specified by idn and schema
+    afterwards save it to file at path
+    """
+    url = address(idn)
+    fp = idn + ".json"
+    fp = path + "/" + fp
+    urlretrieve(url, fp)
