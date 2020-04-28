2bf8c61cb2ddef748e2ac42b04b59d286b09467f
herreio <git@herre.io>
2020-04-28 12:59:36 +0200

    update version (2020.04.28)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2020, Donatus Herre'
 author = 'Donatus Herre'
 
 # The short X.Y version
-version = '2020.04.26'
+version = '2020.04.28'
 # The full version, including alpha/beta/rc tags
-release = '2020.04.26'
+release = '2020.04.28'
 
 
 # -- General configuration ---------------------------------------------------
