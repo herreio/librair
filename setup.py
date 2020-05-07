3c75c1dcb5fff600f3c6b79a9933a69d599514e8
herreio <git@herre.io>
2020-05-07 17:03:56 +0200

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="librair",
-    version="2020.5.7",
+    version="2020.5.8",
     author="Donatus Herre",
     author_email="pypi@herre.io",
     license="MIT",
