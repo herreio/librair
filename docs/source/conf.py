fada6d1ef5a03224f5f8e10af925aff36a2ce88f
herreio <git@herre.io>
2020-05-07 12:50:41 +0200

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2020, Donatus Herre'
 author = 'Donatus Herre'
 
 # The short X.Y version
-version = '2020.4.29'
+version = '2020.5.7'
 # The full version, including alpha/beta/rc tags
-release = '2020.4.29'
+release = '2020.5.7'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="librair",
-    version="2020.4.29",
+    version="2020.5.7",
     author="Donatus Herre",
     author_email="pypi@herre.io",
     license="MIT",
