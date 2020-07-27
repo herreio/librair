d3a2718bbea50b81ff6966d5769c82b29e0a0bee
herreio <git@herre.io>
2020-07-27 15:44:52 +0200

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2020, Donatus Herre'
 author = 'Donatus Herre'
 
 # The short X.Y version
-version = '2020.7.2'
+version = '2020.7.3'
 # The full version, including alpha/beta/rc tags
-release = '2020.7.2'
+release = '2020.7.3'
 
 
 # -- General configuration ---------------------------------------------------
--- librair/__init__.py
+++ librair/__init__.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Copyright (c) 2020  Donatus Herre <git@herre.io>
+
+This module offers tools to harvest data from different library catalogs.
+"""
+__author__ = "Donatus Herre <git@herre.io>"
+__version__ = "2020.7.3"
+__license__ = "MIT"
 
 from . import interfaces
 from . import protocols
